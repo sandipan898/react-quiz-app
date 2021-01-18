018b8e3a773f6d7692cd75c04aa9dcc194daa24b
sandipan898 <sandipan.das898@gmail.com>
2021-01-18 21:50:56 +0530

    setting up question API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import FlashcardList from './components/FlashcardList';
 import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  useEffect(() => {
+    
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api.php', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
       .then(res => {
         // console.log(res.data)
         setFlashcards(res.data.results.map((questionItem, index) => {
@@ -24,11 +46,32 @@ function App() {
           }
         }))
       })
-  }, [])
+  }
+
   return (
-    <div className='container'>
-      <FlashcardList flashcards={flashcards} />
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+            <label htmlFor="amount">Number of questions</label>
+            <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+        
+      </form>
+      <div className='container'>
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,20 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState('initial')
+
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 200))
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    }, [])
 
     return (
-        <div onClick={() => setFlip(!flip)} className={`card ${flip ? `flip` : ``}`}>
-            {/* <button onClick={() => setFlip(!flip)}>Flip</button> */}
-            <div className="front">
+        <div 
+            onClick={() => setFlip(!flip)} 
+            className={`card ${flip ? `flip` : ``}`}
+            style={{ height: height }}
+            >
+            <div className="front" ref={frontEl}>
                 {flashcard.question} 
                 <div className="flashcard-options">
                     {flashcard.options.map(option => {
-                        return <div className="flashcard-option">{option}</div>
+                        return <div className="flashcard-option" key={option} >{option}</div>
                     })}
                 </div>
             </div>
-            <div className="back">{flashcard.answer}</div>
+            <div className="back" ref={backEl}>{flashcard.answer}</div>
         </div>
     )
 }
