799a577109ffe6ab4ab131a42f8f36084d2d1f26
sandipan898 <sandipan.das898@gmail.com>
2021-01-18 17:39:57 +0530

    setting up question API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FlashcardList from './components/FlashcardList';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-  
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        // console.log(res.data)
+        res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer
+          const options = [
+            ...questionItem.incorrect_answers, answer
+          ]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: questionItem.question,
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        })
+      })
+  }, [])
   return (
     <FlashcardList flashcards={flashcards} />
   );
@@ -33,6 +53,6 @@ const SAMPLE_FLASHCARDS = [
       'Answer 3'
     ]
   },
-  
+
 ]
 export default App;
